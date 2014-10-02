b3ff124ae1fe9b39f9d9f30255134f53ca330335
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2014-10-02 19:13:00 +0200

    Makes 'make .gen-man' compiles

--- man/man.go
+++ man/man.go
@@ -5,8 +5,5 @@ import (
 )
 
 func main() {
-	parser.ApplicationName = "autobuild"
-
-	parser.WriteManPage(os.Stdout,
-		"`autobuild' is an easy to use debian package build system. It's main purpose is to simplify the process of maintaing various pieces of the debian package build system (mainly `pbuilder' and `reprepro'). `autobuild' provides a single entry point to managing the build process.")
+	parser.WriteManPage(os.Stdout)
 }
